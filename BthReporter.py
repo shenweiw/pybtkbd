7fc5ac19120fd03bfe9d603028003f15da9aab86
shenweiw <shenweiw@gmail.com>
2014-03-13 12:28:04 +0800

    Git command Test 2

--- BthReporter.py
+++ BthReporter.py
@@ -7,6 +7,7 @@ import logging
 import binascii
 import sdp_helper
 #add sdp_helper to send out the HID descriptors.
+#Continue to practise with Git command.
 
 # reverse from hidkey_to_linuxkey.
 linuxkey_to_hidkey = [
